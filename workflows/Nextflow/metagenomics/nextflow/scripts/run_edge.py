c59e2953ce9e9058d9ec0a8fb17cc22533dec245
mflynn <mflynn@lanl.gov>
2026-05-15 08:47:40 -0600

    change how nextflow is called, use nf_functions.py

--- workflows/Nextflow/metagenomics/nextflow/scripts/run_edge.py
+++ workflows/Nextflow/metagenomics/nextflow/scripts/run_edge.py
@@ -5,33 +5,46 @@ from pathlib import Path
 import os
 
 from config_nextflow import render_nextflow_config
-from nf_functions import start_job
+from nf_functions import start_job, query_job_status
 
 def run_nextflow_pipeline(projects_dir:Path, conf_json_file:Path, project_name:str, 
                           nextflowOutDir:Path, refdata_dir:Path, opaver_web_dir:Path, template_file: Path, 
-                          paired=None, fastq_files=None) -> None:
+                          paired:bool=None, fastq_files:str=None, project_code:str=None) -> None:
     """
     Renders the Nextflow config file and runs the Nextflow pipeline.
     """
-    project_code = render_nextflow_config(projects_dir, conf_json_file, project_name, nextflowOutDir,
+    if project_code is None:
+        project_code = render_nextflow_config(projects_dir, conf_json_file, project_name, nextflowOutDir,
                            refdata_dir, opaver_web_dir, template_file, paired, fastq_files)
-    config_path = projects_dir / project_code / 'nextflow.config'
-    call_nextflow_run(config_path)
-
-
-def call_nextflow_run(config_path:Path, project_code) -> None:
-    """
-    Calls the Nextflow pipeline with the provided config file path.
-    """
+    
     root_dir = Path.cwd().parent.parent.parent
     main_nf_path = str(root_dir / 'workflows/Nextflow/metagenomics/nextflow/main.nf')
-    start_job(job_name=project_code, project_path=main_nf_path, run_dir=root_dir)
+    start_job(job_name=project_code, project_path=main_nf_path, run_dir=projects_dir / project_code, temp_dir=nextflowOutDir / project_code)
+
+def query_job_status(projects_dir:Path, project_code:str) -> str:
+    """
+    Queries the status of a Nextflow job by checking the existence of the `nextflow.log` file in the project directory.
+
+    Parameters
+    ----------
+    projects_dir : Path
+        Path to the directory containing all project output directories.
+    project_code : str
+        Project code for the Nextflow job, which corresponds to the name of the project output directory.
+
+    Returns
+    -------
+    str
+        Status of the Nextflow job, which can be "running", "completed", or "not found".
+    """
+    status = query_job_status(projects_dir, project_code)
+    return status
 
 
 def main():
     parser = argparse.ArgumentParser(description="run EDGE Nextflow pipeline")
-    parser.add_argument("--project-name", type=str, help="Project name chosen by user")
-    parser.add_argument("--fastq-file", type=str, help="Path to FASTQ file containing raw reads to be processed")
+    parser.add_argument("--project-name", type=str, help="Project name chosen by user")    
+    parser.add_argument("--project-code", type=str, default=None, help="Project code for an existing Nextflow config file (if not provided, a new one will be rendered from the template and config JSON file)")
     parser.add_argument("--conf-json-file", type=Path, help="Path to config JSON file used to define pipeline parameters and modules to run")
     parser.add_argument("--paired", action="store_true", help="Indicates if the input files are paired-end")
     parser.add_argument("--fastq-files", type=str, help="Comma-separated list of paths to FASTQ files containing raw reads to be processed "
@@ -41,7 +54,7 @@ def main():
     parser.add_argument("--refdata-dir", type=Path, help="Path to reference data directory", default=os.environ.get('REFDATA_DIR'))
     parser.add_argument("--opaver-web-dir", type=Path, help="Path to OPAVER web directory", default=os.environ.get('OPAVER_WEB_DIR'))
     parser.add_argument("--template-file", type=Path, help="Path to template file used to render Nextflow config file", default=os.environ.get('TEMPLATE_FILE'))
-    parser.add_argument("--nextflow-config-file", type=Path, help="Path to Nextflow config file (if not provided, it will be rendered from the template and config JSON file)")
+
 
     args = parser.parse_args()
 
@@ -53,14 +66,12 @@ def main():
     opaver_web_dir = args.opaver_web_dir
     template_file = args.template_file
 
-    if args.nextflow_config_file:
-        # If a Nextflow config file is provided, use it directly
-        call_nextflow_run(args.nextflow_config_file)
-    else:
-        run_nextflow_pipeline(projects_dir, conf_json_file, project_name, nextflowOutDir,
+    
+    run_nextflow_pipeline(projects_dir, conf_json_file, project_name, nextflowOutDir,
                                refdata_dir, opaver_web_dir, template_file, 
                                paired=None if args.paired is None else args.paired, 
-                               fastq_files=None if args.fastq_files is None else args.fastq_files.split(','))
+                               fastq_files=None if args.fastq_files is None else args.fastq_files.split(','), 
+                               project_code=args.project_code)
 
 if __name__ == "__main__":
        main()
