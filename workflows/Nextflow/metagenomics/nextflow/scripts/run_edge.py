0aa79f3af4bb768493318d34f650b489316e04ca
mflynn <mflynn@lanl.gov>
2026-05-18 17:08:11 -0600

    add project_code to log path for qurey_job_status

--- workflows/Nextflow/metagenomics/nextflow/scripts/run_edge.py
+++ workflows/Nextflow/metagenomics/nextflow/scripts/run_edge.py
@@ -87,7 +87,7 @@ def main():
 
         if args.project_code is None:
             sys.exit("To query the status of an existing Nextflow job, please provide the project code using the --project-code argument.")
-        status = query_job_status(projects_dir, args.project_code)
+        status = query_job_status(projects_dir / args.project_code, args.project_code)
         logging.info(f"Status of Nextflow job with project code {args.project_code}: {status}")
         sys.exit(0)
     run_nextflow_pipeline(projects_dir, conf_json_file, project_name, nextflowOutDir,
