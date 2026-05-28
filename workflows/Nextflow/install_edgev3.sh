84d19e01213ae382a8f8ea1cc639f2d3b3f27166
mflynn <mflynn@lanl.gov>
2026-05-28 10:16:01 -0600

    add more logging statements

--- workflows/Nextflow/install_edgev3.sh
+++ workflows/Nextflow/install_edgev3.sh
@@ -247,8 +247,8 @@ export OPAVER_WEB_DIR="$OPAVER_WEB_DIR"
 export TEMPLATE_FILE="$TEMPLATE_FILE"
 alias run_edge='python $HOME/edge-v3/workflows/Nextflow/metagenomics/nextflow/scripts/run_edge.py'
 EOF
-    echo "Variables saved to .bash_profile"
-    echo "Source it later with: source ~/.bash_profile"
+    log "INFO" "Variables saved to .bash_profile"
+    log "INFO" "Source it later with: source ~/.bash_profile"
 fi
 
 exit 0
